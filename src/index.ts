01acfda69853090f0c19f1391bff3d0c5f50d919
zcong1993 <zhangcong1992@gmail.com>
2021-03-25 22:36:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core'
+import { Octokit } from '@octokit/rest'
 
 async function run(): Promise<void> {
   try {
-    const body = core.getInput('body') || 'default'
-    core.info(`Echo body: ${body}`)
+    const token = core.getInput('token')
+    const base = core.getInput('base') || 'master'
+    const head = core.getInput('head', { required: true })
+    const commit_message = core.getInput('commit_message')
+
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+    const client = new Octokit({ auth: token })
+    await client.repos.merge({
+      repo,
+      owner,
+      base,
+      head,
+      commit_message,
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
